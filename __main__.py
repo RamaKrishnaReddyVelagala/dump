0b6ddddfa22e5ce562b17ed6d8471e0f5e085974
Ram Velagala <kittuvelagala@gmail.com>
2023-06-02 11:26:57 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,1 +1,8 @@
 
+# File "/deployments/venv/lib64/python3.9/site-packages/pandas/io/sql.py", line 480, in read_sql
+# pandas_sql = pandasSQL_builder(con)
+# File "/deployments/venv/lib64/python3.9/site-packages/pandas/io/sql.py", line 656, in pandasSQL_builder
+# return SQLDatabase(con, schema=schema, meta=meta)
+# File "/deployments/venv/lib64/python3.9/site-packages/pandas/io/sql.py", line 1147, in __init__
+# meta = MetaData(self.connectable, schema=schema)
+# TypeError: __init__() got multiple values for argument 'schema'
