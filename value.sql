c5fa4a9c56d84e9036ffbfc3c2ff8a7930efb04a
Ram Velagala <kittuvelagala@gmail.com>
2023-07-25 08:53:01 -0500

    Update value.sql

--- value.sql
+++ value.sql
@@ -1,12 +1,12 @@
 -- Assuming you have a table named 'your_table' with a column 'your_json_column'
-WITH flattened_data AS (
-  SELECT
-    your_json_column:"transcript" AS transcript
-  FROM
-    your_table
-)
 SELECT
-  LISTAGG(entry:"text", ' ') WITHIN GROUP (ORDER BY entry:"index") AS total_text
-FROM
-  flattened_data,
-  LATERAL FLATTEN(input => flattened_data.transcript) entry;
+  LISTAGG(entry_value:"text", ' ') WITHIN GROUP (ORDER BY entry_key) AS total_text
+FROM (
+  SELECT
+    entry.key AS entry_key,
+    entry.value AS entry_value
+  FROM
+    your_table,
+    LATERAL FLATTEN(input => your_json_column:"transcript") entry
+)
+GROUP BY 1;
