3a25ecab7aeed487b2bda65dbb9d53f244e312c4
Ram Velagala <kittuvelagala@gmail.com>
2023-07-25 09:03:35 -0500

    Update value.sql

--- value.sql
+++ value.sql
@@ -1,7 +1,8 @@
 SELECT LISTAGG(transcript_value, ' ') WITHIN GROUP (ORDER BY array_index) AS concatenated_transcript
 FROM (
-  SELECT transcript_obj.array_index,
-         transcript_obj.value:pretty_transcript::string AS transcript_value
+  SELECT 
+    ARRAY_INDEX(transcript_array, transcript_value) AS array_index,
+    transcript_value:pretty_transcript::string AS transcript_value
   FROM your_table,
-       LATERAL FLATTEN(input => parse_json(transcript_column), path => 'transcript') AS transcript_obj
+  LATERAL FLATTEN(input => parse_json(transcript_column), OUTER => true) transcript_array
 );
