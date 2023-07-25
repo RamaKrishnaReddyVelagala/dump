35e20a07e0c458823b92c7e151a9d355e9f645f4
Ram Velagala <kittuvelagala@gmail.com>
2023-07-25 12:09:55 -0500

    Update value.sql

--- value.sql
+++ value.sql
@@ -1,4 +1,1 @@
-SELECT
-  LISTAGG(transcript_obj:pretty_transcript, ' ') AS all_text
-FROM sometable,
-LATERAL FLATTEN(input => parse_json(transcript_column):transcript) transcript_obj;
+json:get(json_column, 'ani')
